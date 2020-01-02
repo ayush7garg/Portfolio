6dbd8da020f7e3c0665bf6f11747fe52738b8622
Ayush Garg <987ayush@gmail.com>
2020-01-02 22:51:24 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #!/usr/bin/env python
 import os
 import sys
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-=======
+
 #!/usr/bin/env python
 import os
 import sys
@@ -30,4 +30,3 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
->>>>>>> cfed5fd48a3ab911abb4902a898f345208cde5ba
